b2b384ccf9996f118d65c7a7cbd6558272036d28
joedaviesio <100094158+joedaviesio@users.noreply.github.com>
2022-05-01 22:27:44 +1200

    upload

--- candidate/js/data.js
+++ candidate/js/data.js
@@ -1,6 +1,6 @@
 let titles = {
-    prime_header: 'Candidate Application',
-    slogan: "with the people of Christchurch",
+    prime_header: 'Tyrone Fields',
+    slogan: "with the people of Banks Peninsula",
     sub1: 'bio',
     sub2: 'policy',
     sub3: 'blog',
@@ -20,7 +20,7 @@ let button_icon = {
 }
 
 let photos = {
-    bio_01: '/candidate/src/img/bio/jake_bio.jpg',
+    bio_01: '/candidate/src/img/bio/tyrone_bio.jpg',
     bio_02: '/candidate/src/img/bio/2021-10-15-red-peak-a.JPG'
 }
 
