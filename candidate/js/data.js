f5de369e2d7ed358a7da31fcbef0216a04db4cbe
joedaviesio <100094158+joedaviesio@users.noreply.github.com>
2022-05-01 21:13:38 +1200

    upload

---
+++ candidate/js/data.js
@@ -0,0 +1,191 @@
+let titles = {
+    prime_header: 'Candidate Application',
+    slogan: "with the people of Christchurch",
+    sub1: 'bio',
+    sub2: 'policy',
+    sub3: 'blog',
+    button1: 'contact'
+}
+
+let nav_icon = {
+    type: 'fa-solid',
+    title: 'fa-bars',
+    size: 'fa-3x'
+}
+
+let button_icon = {
+    type: 'fa-solid',
+    title: 'fa-hand-sparkles',
+    size: 'fa-0.5x'
+}
+
+let photos = {
+    bio_01: '/candidate/src/img/bio/jake_bio.jpg',
+    bio_02: '/candidate/src/img/bio/2021-10-15-red-peak-a.JPG'
+}
+
+let para = {
+
+    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo iure quam perspiciatis? Doloremque vel, facere officia possimus harum voluptatum! Quaerat, suscipit. Voluptates asperiores veniam nobis quibusdam, accusamus, recusandae nostrum tempora expedita esse blanditiis, perferendis eveniet? At doloribus ex porro illum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo iure quam perspiciatis? Doloremque vel, facere officia possimus harum voluptatum! Quaerat, suscipit. Voluptates asperiores veniam nobis quibusdam, accusamus, recusandae nostrum tempora expedita esse blanditiis, perferendis eveniet? At doloribus ex porro illum."
+    
+
+
+}
+
+let dropdown_options = ["<a href=''>home</a>", "bio", "policy", "blog", "socials", "contact"];
+
+let sub_header_options = ["bio", "policy", "blog"];
+
+let list_icon = {
+
+    type: 'fa-solid',
+    title: 'fa-circle-check',
+    size: 'fa-2x'
+
+
+}
+
+let list_1 = {
+    title: 'no asset sales',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, iusto repellendus magni obcaecati ratione doloribus possimus temporibus reiciendis laudantium. Adipisci, vel consequuntur hic cumque, qui odit aliquam officia a impedit, repellendus fugit reiciendis debitis tempore.',
+    icon_type: list_icon.type, 
+    icon_title: list_icon.title,
+    icon_size: list_icon.size
+}
+let list_2 = {
+    title: 'funding for community facilities',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, iusto repellendus magni obcaecati ratione doloribus possimus temporibus reiciendis laudantium. Adipisci, vel consequuntur hic cumque, qui odit aliquam officia a impedit, repellendus fugit reiciendis debitis tempore.',
+    icon_type: list_icon.type, 
+    icon_title: list_icon.title,
+    icon_size: list_icon.size
+}
+
+let list_3 = {
+    title: 'policy 3',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, iusto repellendus magni obcaecati ratione doloribus possimus temporibus reiciendis laudantium. Adipisci, vel consequuntur hic cumque, qui odit aliquam officia a impedit, repellendus fugit reiciendis debitis tempore.',
+    icon_type: list_icon.type, 
+    icon_title: list_icon.title,
+    icon_size: list_icon.size
+}
+
+let list_4 = {
+    title: 'policy 4',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, iusto repellendus magni obcaecati ratione doloribus possimus temporibus reiciendis laudantium. Adipisci, vel consequuntur hic cumque, qui odit aliquam officia a impedit, repellendus fugit reiciendis debitis tempore.',
+    icon_type: list_icon.type, 
+    icon_title: list_icon.title,
+    icon_size: list_icon.size
+}
+let list_5 = {
+    title: 'policy 5',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, iusto repellendus magni obcaecati ratione doloribus possimus temporibus reiciendis laudantium. Adipisci, vel consequuntur hic cumque, qui odit aliquam officia a impedit, repellendus fugit reiciendis debitis tempore.',
+    icon_type: list_icon.type, 
+    icon_title: list_icon.title,
+    icon_size: list_icon.size
+}
+
+let list_total = [list_1, list_2, list_3, list_4, list_5];
+
+console.log(list_total[0]);
+
+let blog_icon = {
+
+    type: 'fa-solid',
+    title: 'fa-comment',
+    size: 'fa-2x'
+
+
+}
+
+let photo_icon = {
+    type: 'fa-solid',
+    title: 'fa-camera',
+    size: 'fa-2x'
+}
+// blog 
+
+let blog_2 = {
+    title: '09 August 10:18',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, iusto repellendus magni obcaecati ratione doloribus possimus temporibus reiciendis laudantium. Adipisci, vel consequuntur hic cumque, qui odit aliquam officia a impedit, repellendus fugit reiciendis debitis tempore.',
+    blog_img_boolean: 'false',
+    icon_type: blog_icon.type, 
+    icon_title: blog_icon.title,
+    icon_size: blog_icon.size
+}
+
+
+let blog_4 = {
+    title: '08 October 12:56',
+    text: 'Otira scree.',
+    blog_img_boolean: 'true',
+    blog_img_src: '/candidate/src/img/blog/2021-10-08-Otira-scree-b.JPG',
+    icon_type: blog_icon.type, 
+    icon_title: photo_icon.title,
+    icon_size: blog_icon.size
+}
+
+
+
+let blog_7 = {
+    title: '24 November 13:22',
+    text: 'Everythings gonna be alright.',
+    blog_img_boolean: 'true',
+    blog_img_src: '/candidate/src/img/blog/2021-11-24-everything-alright.jpg',
+    icon_type: blog_icon.type, 
+    icon_title: photo_icon.title,
+    icon_size: blog_icon.size
+}
+
+
+let blog_9 = {
+    title: '25 December 10:18',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, iusto repellendus magni obcaecati ratione doloribus possimus temporibus reiciendis laudantium. Adipisci, vel consequuntur hic cumque, qui odit aliquam officia a impedit, repellendus fugit reiciendis debitis tempore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, iusto repellendus magni obcaecati ratione doloribus possimus temporibus reiciendis laudantium. Adipisci, vel consequuntur hic cumque, qui odit aliquam officia a impedit, repellendus fugit reiciendis debitis tempore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, iusto repellendus magni obcaecati ratione doloribus possimus temporibus reiciendis laudantium. Adipisci, vel consequuntur hic cumque, qui odit aliquam officia a impedit, repellendus fugit reiciendis debitis tempore.',
+    blog_img_boolean: 'false',
+    icon_type: blog_icon.type, 
+    icon_title: blog_icon.title,
+    icon_size: blog_icon.size
+}
+
+
+
+
+
+let blog_total = [ blog_2,blog_4, blog_7, blog_9];
+
+
+let insta_icon = {
+    type: 'fa-brands',
+    title: 'fa-instagram',
+    size: 'fa-3x'
+
+}
+
+let fb_icon = {
+    type: 'fa-brands',
+    title: 'fa-facebook',
+    size: 'fa-3x'
+
+}
+
+let socials_1 = {
+    href: "https://www.instagram.com",
+
+    icon_type: insta_icon.type, 
+    icon_title: insta_icon.title,
+    icon_size: insta_icon.size
+}
+
+let socials_2 = {
+    href: "https://www.facebook.com",
+
+    icon_type: fb_icon.type, 
+    icon_title: fb_icon.title,
+    icon_size: fb_icon.size
+}
+
+let socials_total = [socials_1, socials_2];
+
+let video_list = {
+    techno_video: '<iframe width="100%" src="https://www.youtube.com/embed/C-SFjuHTwWs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+
+}
