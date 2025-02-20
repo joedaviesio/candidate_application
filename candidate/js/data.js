ac2d8a33bacfff6a14690c9b2fc7fc8e19d7aa56
joedaviesio <100094158+joedaviesio@users.noreply.github.com>
2025-02-20 15:35:58 +1300

    update

--- candidate/js/data.js
+++ candidate/js/data.js
@@ -616,8 +616,18 @@ let blog_55 = {
     icon_size: blog_icon.size
 }
 
+let blog_56 = {
+    title: '19 February 11:11 2025 ',
+    text: "To increase bus ridership, we must make public transport a more attractive alternative to cars. Our network review should focus on strategies and routes that enhance the competitive advantages of buses compared to private motor vehicles.",
+    blog_img_boolean: 'true',
+    blog_img_src: 'candidate/src/img/bio/2025-02-19-press-img.jpg',
+    icon_type: blog_icon.type, 
+    icon_title: photo_icon.title,
+    icon_size: blog_icon.size
+}
 
-let blog_total = [ blog_9, blog_10, blog_11, blog_12, blog_13, blog_14, blog_15, blog_16, blog_17, blog_18, blog_19, blog_20, blog_21, blog_22, blog_23, blog_24, blog_25, blog_26, blog_27, blog_28, blog_29, blog_30, blog_31, blog_32, blog_33, blog_34, blog_35, blog_36, blog_37, blog_38, blog_39, blog_40, blog_41, blog_42, blog_43, blog_44, blog_45, blog_46, blog_47, blog_48, blog_49, blog_50, blog_51, blog_52, blog_53, blog_54, blog_55];
+
+let blog_total = [ blog_9, blog_10, blog_11, blog_12, blog_13, blog_14, blog_15, blog_16, blog_17, blog_18, blog_19, blog_20, blog_21, blog_22, blog_23, blog_24, blog_25, blog_26, blog_27, blog_28, blog_29, blog_30, blog_31, blog_32, blog_33, blog_34, blog_35, blog_36, blog_37, blog_38, blog_39, blog_40, blog_41, blog_42, blog_43, blog_44, blog_45, blog_46, blog_47, blog_48, blog_49, blog_50, blog_51, blog_52, blog_53, blog_54, blog_55, blog_56];
 
   
 
